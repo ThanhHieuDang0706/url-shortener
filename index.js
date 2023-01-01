763ed5c3b28ab907045a0e013959d1b397ba5793
Hieu Dang <hieu.dang.dth_is_me@hcmut.edu.vn>
2023-01-01 23:01:39 +0700

    add mongo

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const express = require("express");
+require("dotenv").config();
 const mongoose = require("mongoose");
+const ShortUrl = require("./models/shortUrl.js");
+
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGO_URL || "mongodb://localhost:3000/shortUrl", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.post("/short-url", (req, res) => {});
+app.post("/short-url", async (req, res) => {
+  await ShortUrl.create({
+    full: req.body.fullUrl,
+  });
+  res.redirect("/");
+});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server listening on port ${process.env.PORT || 3000}`);
